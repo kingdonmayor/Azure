c68ba3ca6aab5fc14e04132af580b86a83d9d48d
kingdonmayor <donmayo2010@hotmail.com>
2024-01-03 08:32:23 +0100

    Add files via upload

---
+++ servicebusscript.py
@@ -0,0 +1,34 @@
+from azure.servicebus import ServiceBusClient
+from azure.servicebus import ServiceBusClient, ServiceBusSubQueue
+from azure.servicebus import ServiceBusClient, ServiceBusSender
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from azure.servicebus import ServiceBusClient, ServiceBusReceiver
+from azure.servicebus import ServiceBusClient, ServiceBusReceivedMessage
+import json
+import os
+connectionString = "Endpoint=sb://abujaservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=6KkaUPtKIIqTOiAbCc0aDv3pY7QRW8vCpO53HyNxu9k="
+serviceBusClient = ServiceBusClient.from_connection_string(connectionString)
+queueName = "createcifandaccount"
+Client = serviceBusClient.get_queue_sender(queueName)
+queue_receiver = serviceBusClient.get_queue_receiver(queueName)
+messageToSend=[]
+with serviceBusClient.get_queue_receiver(queueName, sub_queue=ServiceBusSubQueue.DEAD_LETTER, prefetch_count=500) as queueReceiver:
+
+    messages = queueReceiver.receive_messages(max_message_count=500, max_wait_time=4000)
+    msgLength = len(messages)
+    
+    while msgLength > 0:
+        for message in messages:
+            body=next(message.body)
+            messageToSend.append(body)
+            print(str(message))
+            queueReceiver.complete_message(message)
+    
+        
+        with  serviceBusClient.get_queue_sender(queueName) as sender:
+            for message in messageToSend:
+                newmessage=ServiceBusMessage(message)
+                sender.send_messages(newmessage)
+        
+        messages = queueReceiver.receive_messages(max_message_count=500, max_wait_time=4000)
+        msgLength = len(messages)
